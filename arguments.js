c51a4d7aabbb86dfbd6b40a902eee24918629fbd
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-03 00:16:14 +0600

    arguments vs parameter

---
+++ arguments.js
@@ -0,0 +1,12 @@
+function double(num) // parameter: num
+{
+    const double = num * 2;
+    console.log(num, double);
+}
+
+console.log('I will call the function')
+double(43); // argument: 43
+console.log('-----------');
+
+double(62); // argument: 62
+console.log('------------');
