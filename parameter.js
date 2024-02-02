766eb7808f6206bb3ad1856e1bedd89d4c177d25
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-03 00:09:01 +0600

    function with parameter

---
+++ parameter.js
@@ -0,0 +1,19 @@
+// function declaration
+function vatKhao() {
+    console.log('hand wash kore asho');
+    console.log('bosho');
+    console.log('plate a khabar nao');
+    console.log('ghopat ghopat kore khao', '\n');
+}
+
+// function call
+vatKhao();
+
+
+// square function
+function square (num) {
+    console.log(`square of ${num}: `);
+    return num * num;
+}
+
+console.log(square(4));
