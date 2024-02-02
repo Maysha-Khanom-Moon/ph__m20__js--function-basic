50284365527befffe59137e4afe295da2280f664
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-03 01:03:46 +0600

    another way to declare a function

--- function.js
+++ function.js
@@ -11,8 +11,16 @@ const money = 45;
 function fanOffKor() {
     console.log('bosha theke uthe dara');
     console.log('walk towards the switch');
-    console.log('click the switch to off the Fan');
+    console.log('click the switch to off the Fan\n');
 }
 
 // call the function
 fanOffKor();
+
+
+// another way to declare function
+let myfunc = function() {
+    return 2;
+}
+
+console.log(myfunc());
