34e8f3e431f695f372f02aeabea578161a63eca1
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-02 22:39:40 +0600

    function declaration and function call

---
+++ function.js
@@ -0,0 +1,18 @@
+/**
+ * ----- function declaration -----
+ * 
+ * function funcName() {
+ *      statement
+ * }
+ */
+
+const money = 45;
+
+function fanOffKor() {
+    console.log('bosha theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off the Fan');
+}
+
+// call the function
+fanOffKor();
